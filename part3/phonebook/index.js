895e1ff69065b9536216a62b8de158297603fc4c
skrambol <systemplado@gmail.com>
2023-11-09 11:58:18 +0800

    part 3.7, 3.8: added morgan logging

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
-
+const morgan = require('morgan')
 const app = express()
+
+morgan.token('body', req => {
+  return JSON.stringify(req.body)
+})
+
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 let phonebook = [
   {
     "id": 1,
