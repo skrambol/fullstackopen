e1a0d5cbc0a430bd3f1e162ee1d397275edb820b
skrambol <systemplado@gmail.com>
2023-11-09 11:32:08 +0800

    part 3.3: GET /api/persons/:id

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -28,6 +28,17 @@ app.get('/api/persons', (req, res) => {
   res.json(phonebook)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = phonebook.find(p => p.id === id)
+
+  if (person) {
+    return res.json(person)
+  }
+
+  return res.status(404).end()
+})
+
 app.get('/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${phonebook.length} people</p><p>${new Date()}</p>`)
 })
