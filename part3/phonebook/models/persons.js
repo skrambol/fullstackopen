03e94210a6aeb0188fc42115f5fac681461defcd
skrambol <systemplado@gmail.com>
2023-11-10 19:06:50 +0800

    part 3.20: number validation

--- part3/phonebook/models/persons.js
+++ part3/phonebook/models/persons.js
@@ -18,6 +18,12 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d+/.test(v)
+      }
+    },
     required: true,
   }
 })
