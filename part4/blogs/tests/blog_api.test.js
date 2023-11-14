c407c8be08ddfd37dc07879824d0bc5d537d270b
skrambol <systemplado@gmail.com>
2023-11-14 18:11:19 +0800

    part 4.10 POST /api/blogs tests - added test_helper.js

--- part4/blogs/tests/blog_api.test.js
+++ part4/blogs/tests/blog_api.test.js
@@ -1,65 +1,16 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const {initialBlogs, blogsInDb} = require('./test_helper')
 const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-const BLOGS = [
-  {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: "5a422b891b54a676234d17fa",
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: "5a422ba71b54a676234d17fb",
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: "5a422bc61b54a676234d17fc",
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2,
-    __v: 0
-  }
-]
 
 beforeEach(async () => {
   await Blog.deleteMany({})
 
-  const promisedBlogs = BLOGS.map(blog => new Blog(blog).save())
+  const promisedBlogs = initialBlogs.map(blog => new Blog(blog).save())
   await Promise.all(promisedBlogs)
 })
 
@@ -67,14 +18,22 @@ describe('/api/blogs', () => {
   test('GET gets all blogs', async () => {
     const response = await api.get('/api/blogs')
 
-    expect(response.body).toHaveLength(BLOGS.length)
-  }, 10000)
+    expect(response.body).toHaveLength(initialBlogs.length)
+  })
+
+  test('GET blogs have "id" property', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body[0].id).toBeDefined()
+  })
 
   test('POST creates new blog', async () => {
     const newBlog = {title: "newest title", author: "O. Thor", url: "http://example.org/newest-title", likes: 2}
     const response = await api.post('/api/blogs', newBlog)
 
+    const allBlogs = await blogsInDb()
     expect(response.statusCode).toBe(201)
+    expect(allBlogs).toHaveLength(initialBlogs.length + 1)
   })
 })
 
