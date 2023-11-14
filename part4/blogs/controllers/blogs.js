734125c6a5e7ab71844baf2db0397e711bf6caf2
skrambol <systemplado@gmail.com>
2023-11-14 12:35:18 +0800

    part 4.2: refactored models and controllers

---
+++ part4/blogs/controllers/blogs.js
@@ -0,0 +1,24 @@
+const Blog = require('../models/blog')
+const router = require('express').Router()
+
+router.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+    .catch(error => next(error))
+})
+
+router.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = router
--- part4/blogs/index.js
+++ part4/blogs/index.js
@@ -4,15 +4,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const middleware = require('./utils/middleware')
 const {MONGODB_URI, PORT} = require('./utils/config')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(MONGODB_URI)
 .then(() => {
@@ -26,23 +18,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.errorHandler)
 app.use(middleware.unknownEndpoint)
